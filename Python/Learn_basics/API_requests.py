29c1fe971e480d0dc9d27f2266392916c004e810
Ivan Pavlov <pavlovis95@yandex.ru>
2023-06-16 17:29:46 +0300

    A little refactoring this repo

--- Python/Learn_basics/API_requests.py
+++ Python/Learn_basics/API_requests.py
@@ -1,8 +1,7 @@
 import requests
 
-response = requests.get("https://github.com/BigYorlde/python-scripts.git")
+response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
 my_projects = response.json()
 
 for project in my_projects:
     print(f"Project name: {project['name']} \nProject Url: {project['web_url']}")
-
